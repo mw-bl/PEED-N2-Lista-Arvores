cf1ebcfa163a806af32943def21f50dae2b31523
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-09-05 16:32:58 -0300

    :sparkles: feat: Added Q1/Q2

--- tree_lista.py
+++ tree_lista.py
@@ -1,3 +1,41 @@
 # Marcos Willian :D
 
-print("Olá")
+# Q1 / Q2
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+class ArvoreBinaria:
+    def __init__(self):
+        self.raiz = None
+
+    def inserir_em_nivel(self, valor):
+        if self.raiz is None:
+            self.raiz = No(valor)
+        else:
+            self.inserir_em_nivel_recursivo(valor, self.raiz)
+
+    def inserir_em_nivel_recursivo(self, valor, no):
+        if valor < no.valor:
+            if no.esquerda is None:
+                no.esquerda = No(valor)
+            else:
+                self.inserir_em_nivel_recursivo(valor, no.esquerda)
+        else:
+            if no.direita is None:
+                no.direita = No(valor)
+            else:
+                self.inserir_em_nivel_recursivo(valor, no.direita)
+
+
+# Testes das Questões
+arvore = ArvoreBinaria()
+arvore.inserir_em_nivel(5)
+arvore.inserir_em_nivel(3)
+arvore.inserir_em_nivel(7)
+arvore.inserir_em_nivel(2)
+arvore.inserir_em_nivel(4)
+arvore.inserir_em_nivel(6)
+arvore.inserir_em_nivel(8)
